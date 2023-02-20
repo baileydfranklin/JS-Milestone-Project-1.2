3125962152432f7c5a534007c34e1220a6e74bfd
Bailey Franklin <baileydfranklin@gmail.com>
2023-02-20 15:17:30 -0700

    Added: draw random card and display random card. Changed display deck card sizes to fit face value cards width/heigth

--- deck.js
+++ deck.js
@@ -1,158 +1,158 @@
 const deck = [
     {
-        name: "AC", value: 10
+        name: "AC", value: 10 // 0
     },
     {
-        name: "2C", value: 2
+        name: "2C", value: 2 // 1
     },
     {
-        name: "3C", value: 3
+        name: "3C", value: 3 // 2
     },
     {
-        name: "4C", value: 4
+        name: "4C", value: 4 // 3
     },
     {
-        name: "5C", value: 5
+        name: "5C", value: 5 // 4
     },
     {
-        name: "6C", value: 6
+        name: "6C", value: 6 // 5
     },
     {
-        name: "7C", value: 7
+        name: "7C", value: 7 // 6
     },
     {
-        name: "8C", value: 8
+        name: "8C", value: 8 // 7
     },
     {
-        name: "9C", value: 9
+        name: "9C", value: 9 // 8
     },
     {
-        name: "10C", value: 10
+        name: "10C", value: 10 // 9
     },
     {
-        name: "JC", value: 10
+        name: "JC", value: 10 // 10
     },
     {
-        name: "QC", value: 10
+        name: "QC", value: 10 // 11
     },
     {
-        name: "KC", value: 10
+        name: "KC", value: 10 // 12
     },
     {
-        name: "AD", value: 10
+        name: "AD", value: 10 // 13
     },
     {
-        name: "2D", value: 2
+        name: "2D", value: 2 // 14
     },
     {
-        name: "3D", value: 3
+        name: "3D", value: 3 // 15
     },
     {
-        name: "4D", value: 4
+        name: "4D", value: 4 // 16
     },
     {
-        name: "5D", value: 5
+        name: "5D", value: 5 // 17
     },
     {
-        name: "6D", value: 6
+        name: "6D", value: 6 // 18
     },
     {
-        name: "7D", value: 7
+        name: "7D", value: 7 // 19
     },
     {
-        name: "8D", value: 8
+        name: "8D", value: 8 // 20
     },
     {
-        name: "9D", value: 9
+        name: "9D", value: 9 // 21
     },
     {
-        name: "10D", value: 10
+        name: "10D", value: 10 // 22
     },
     {
-        name: "JD", value: 10
+        name: "JD", value: 10 // 23
     },
     {
-        name: "QD", value: 10
+        name: "QD", value: 10 // 24
     },
     {
-        name: "KD", value: 10
+        name: "KD", value: 10 // 25
     },
     {
-        name: "AH", value: 10
+        name: "AH", value: 10 // 26
     },
     {
-        name: "2H", value: 2
+        name: "2H", value: 2 // 27
     },
     {
-        name: "3H", value: 3
+        name: "3H", value: 3 // 28
     },
     {
-        name: "4H", value: 4
+        name: "4H", value: 4 // 29
     },
     {
-        name: "5H", value: 5
+        name: "5H", value: 5 // 30
     },
     {
-        name: "6H", value: 6
+        name: "6H", value: 6 // 31
     },
     {
-        name: "7H", value: 7
+        name: "7H", value: 7 // 32
     },
     {
-        name: "8H", value: 8
+        name: "8H", value: 8 // 33
     },
     {
-        name: "9H", value: 9
+        name: "9H", value: 9 // 34
     },
     {
-        name: "10H", value: 10
+        name: "10H", value: 10 // 35
     },
     {
-        name: "JH", value: 10
+        name: "JH", value: 10 // 36
     },
     {
-        name: "QH", value: 10
+        name: "QH", value: 10 // 37
     },
     {
-        name: "KH", value: 10
+        name: "KH", value: 10 // 38
     },
     {
-        name: "AS", value: 10
+        name: "AS", value: 10 // 39
     },
     {
-        name: "2S", value: 2
+        name: "2S", value: 2 // 40
     },
     {
-        name: "3S", value: 3
+        name: "3S", value: 3 // 41
     },
     {
-        name: "4S", value: 4
+        name: "4S", value: 4 // 42
     },
     {
-        name: "5S", value: 5
+        name: "5S", value: 5 // 43
     },
     {
-        name: "6S", value: 6
+        name: "6S", value: 6 // 44
     },
     {
-        name: "7S", value: 7
+        name: "7S", value: 7 // 45
     },
     {
-        name: "8S", value: 8
+        name: "8S", value: 8 // 46
     },
     {
-        name: "9S", value: 9
+        name: "9S", value: 9 // 47
     },
     {
-        name: "10S", value: 10
+        name: "10S", value: 10 // 48
     },
     {
-        name: "JS", value: 10
+        name: "JS", value: 10 // 49
     },
     {
-        name: "QS", value: 10
+        name: "QS", value: 10 // 50
     },
     {
-        name: "KS", value: 10
+        name: "KS", value: 10 // 51
     },
 ];
